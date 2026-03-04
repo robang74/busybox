441f78b0a15baeb1e2ef740953777812cf2c5cb2
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-03-04 23:24:02 +1100

    Update __init__.py

--- tools/build/__init__.py
+++ tools/build/__init__.py
@@ -1,1 +1,27 @@
+import pathlib
 
+from .busybox import BusyBoxBuilder
+from .xz import XZBuilder
+from .radare2 import Radare2Builder
+from .platformio import PlatformIOBuilder
+from .cmake import CMakeBuilder
+from .make import MakeBuilder
+
+
+BUILDERS = [
+    BusyBoxBuilder(),
+    XZBuilder(),
+    Radare2Builder(),
+    PlatformIOBuilder(),
+    CMakeBuilder(),
+    MakeBuilder()
+]
+
+
+def detect_builder(project_root):
+
+    for builder in BUILDERS:
+        if builder.detect(project_root):
+            return builder
+
+    return MakeBuilder()
