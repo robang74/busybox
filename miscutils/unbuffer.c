38c9041968a0a3116da0cd1e3e42bbe8590332e1
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-03-21 06:32:39 +0100

    unbuffer: applet to unbuffer applications stdout/err
    /*
     * (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, GPLv2 license
     */
    
    //config:config UNBUFFER
    //config:	bool "unbuffer (0.5kb)"
    //config:	default n
    //config:	help
    //config:	  Run a command in a PTY to disable buffering.
    
    //applet:IF_UNBUFFER(APPLET(unbuffer, BB_DIR_USR_BIN, BB_SUID_DROP))
    
    //kbuild:lib-$(CONFIG_UNBUFFER) += unbuffer.o
    //kbuild:ldflags-$(CONFIG_UNBUFFER) += -lutil
    
    //usage:#define unbuffer_trivial_usage
    //usage: "CMD [ARGS]"
    //usage:#define unbuffer_full_usage "\n\n"
    //usage: "Run a command in a PTY to disable buffering."
    
    /*
      size check: before vs after
         text	   data	    bss	    dec	    hex	filename
      1096595	  16691	   1656	1114942	 11033e	busybox
         text	   data	    bss	    dec	    hex	filename
      1097217	  16711	   1656	1115584	 1105c0	busybox
    
      function                                             old     new   delta
      unbuffer_main                                          -     370    +370
      sigwinch_handler                                      15      98     +83
      packed_usage                                       35565   35602     +37
      applet_names                                        2817    2826      +9
      applet_main                                         3264    3272      +8
      .rodata                                           103410  103418      +8
      ptyfd                                                  -       4      +4
      applet_suid                                          102     103      +1
      applet_install_loc                                   204     205      +1
      ------------------------------------------------------------------------------
      (add/remove: 3/0 grow/shrink: 7/0 up/down: 521/0)             Total: 521 bytes
    */
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ miscutils/unbuffer.c
@@ -0,0 +1,105 @@
+/*
+ * (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, GPLv2 license
+ */
+
+//config:config UNBUFFER
+//config:	bool "unbuffer (0.8kb)"
+//config:	default n
+//config:	help
+//config:	  Run a command in a PTY to disable buffering.
+
+//applet:IF_UNBUFFER(APPLET(unbuffer, BB_DIR_USR_BIN, BB_SUID_DROP))
+
+//kbuild:lib-$(CONFIG_UNBUFFER) += unbuffer.o
+
+//usage:#define unbuffer_trivial_usage
+//usage: "CMD [ARGS]"
+//usage:#define unbuffer_full_usage "\n\n"
+//usage: "Run a command in a PTY to disable buffering."
+
+/*
+  size check: before vs after:
+     text	   data	    bss	    dec	    hex	filename
+  1096595	  16691	   1656	1114942	 11033e	busybox
+     text	   data	    bss	    dec	    hex	filename
+  1097330	  16707	   1664	1115701	 11062c	busybox
+
+  how to apply:
+  copy this file into in miscutils/unbuffer.
+*/
+// how to activate:
+// make oldconfig && sed "s/^# *\(CONFIG_UNBUFFER\).*/\\1=y/" -i .config
+
+#include "libbb.h"
+#include <termios.h>
+#include <unistd.h>
+#include <utmp.h>
+#include <pty.h>
+
+static int ptyfd = -1;
+static pid_t pidfw = 0;
+
+static void sigwinch_handler(int sig UNUSED_PARAM)
+{
+	struct winsize ws;
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0) {
+		ioctl(ptyfd, TIOCSWINSZ, &ws);
+	}
+}
+
+static void forward_signals(int sig) {
+	if (pidfw) kill(pidfw, sig);
+}
+
+#define SIGS_BITMASK ((1<<SIGINT)|(1<<SIGTERM)|(1<<SIGHUP)|(1<<SIGQUIT))
+
+int unbuffer_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
+int unbuffer_main(int argc, char **argv)
+{
+	int n, fd;
+	pid_t pid;
+	struct termios tios;
+	sigset_t set, oldset;
+	char ch;
+
+	if (!(argc >> 1)) bb_show_usage();
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	sigfillset(&set);
+	sigprocmask(SIG_BLOCK, &set, &oldset);
+	signal(SIGWINCH, sigwinch_handler);
+	signal(SIGCHLD, SIG_DFL);
+
+	if((pid = forkpty(&fd, NULL, NULL, NULL)) < 0)
+		bb_perror_msg_and_die("forkpty");
+
+	if (pid == 0) { // child proccess
+		sigprocmask(SIG_SETMASK, &oldset, NULL);
+		BB_EXECVP_or_die(argv + 1);
+	} // parent process
+	ptyfd = fd; pidfw = pid;
+
+	bb_signals(SIGS_BITMASK, forward_signals);
+	sigprocmask(SIG_SETMASK, &oldset, NULL);
+	sigwinch_handler(0);
+
+	if (tcgetattr(ptyfd, &tios) == 0) {
+		cfmakeraw(&tios);
+		tios.c_oflag |= ONLCR;
+		tcsetattr(ptyfd, TCSANOW, &tios);
+	}
+
+	while ((n = read(ptyfd, &ch, 1)) == 1) {
+		if(full_write(STDOUT_FILENO, &ch, 1) != 1) {
+			bb_simple_perror_msg("write");
+			break;
+		}
+	}
+	if (n < 0 && errno != EIO)
+		bb_simple_perror_msg("read");
+
+	close(ptyfd);
+
+	return wait_for_exitstatus(pid);
+}
