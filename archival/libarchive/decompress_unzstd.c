2c976d0b590585bd731d32348693dc9acc736233
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-03-29 17:48:23 +0200

    archival: add support for zstd compressed data
    Status: not yet peer-reviewed, patch did not applied cleanly
    
    zstd compression of tarballs and distribution packages is more
    common.  BusyBox doesn't include zstd code so use external zstd
    and unzstd programs to support it.  Due to the requirement for
    external utilities the feature isn't enabled by default.
    
    With this change BusyBox tar, rpm and dpkg applets can handle
    zstd compressed data.
    
    When the feature is enabled:
    
    function                                             old     new   delta
    setup_transformer_on_fd                              126     192     +66
    tar_main                                            1114    1176     +62
    .rodata                                           102483  102544     +61
    get_header_tar_zstd                                    -      53     +53
    unpack_zstd_stream                                     -      47     +47
    filter_accept_list_reassign                          217     246     +29
    packed_usage                                       35215   35239     +24
    dpkg_deb_main                                        395     415     +20
    unpack_package                                       641     654     +13
    init_archive_deb_control                              75      88     +13
    tar_longopts                                         314     321      +7
    ------------------------------------------------------------------------------
    (add/remove: 4/0 grow/shrink: 9/0 up/down: 395/0)             Total: 395 bytes
    
    Signed-off-by: Ron Yorston <rmy@pobox.com>
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ archival/libarchive/decompress_unzstd.c
@@ -0,0 +1,13 @@
+#include "libbb.h"
+#include "bb_archive.h"
+
+IF_DESKTOP(long long) int FAST_FUNC
+unpack_zstd_stream(transformer_state_t *xstate)
+{
+		// FIXME Provide an internal implementation of zstd decompression.
+		char *argv[] = {(char *)"unzstd", (char *)"-cf", (char *)"-", NULL};
+
+		xmove_fd(xstate->src_fd, STDIN_FILENO);
+		xmove_fd(xstate->dst_fd, STDOUT_FILENO);
+		BB_EXECVP_or_die(argv);
+}
