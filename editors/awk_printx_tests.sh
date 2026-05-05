5e42410c142c0fddf61cc2bf011b154c6498d7e9
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-05-05 14:55:23 +0200

    awk: numeric identifiers full recognition w/ %p in +3b total, p.2
    The intrinsic limitation by "double" (IEEE 754) as type parameter remains for
    integers which are going to lose precision outside the (-2^53, 2^53) range.
    
    ERRATA CORRIGE
    
    Only the first two are valid:
    
    ./busybox awk 'BEGIN { CONVFMT="%Lf p"; x=3.14; print x "" }';
    
    should be
    
    ./busybox awk 'BEGIN { CONVFMT="%lf p"; x=3.14; print x "" }';
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- editors/awk_printx_tests.sh
+++ editors/awk_printx_tests.sh
@@ -34,7 +34,7 @@ echo
 
 # Unit test: see all-and-only the OKs, it is passed otherwise not
 echo
-echo 'Unit test: 3 OKs expected'
+echo 'Unit test: only 5 OKs expected'
 echo
 ./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%.2f OK f"; x=3.14; print x "" }';
@@ -42,6 +42,25 @@ echo
 ./busybox awk 'BEGIN { CONVFMT="walk: %.2fpi %016llx "; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="OK pi: %.2fx"; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="%.2fOKpi"; x=3.14; print x "" }'
+./busybox awk 'BEGIN { CONVFMT="%d OK"; x=3.14; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%f OK p"; x=3.14; print x "" }';
 
-# 3 OKs expected then test $(./run_3oks_test 2>&1 | grep OK | wc -l) -eq 3
+# Unit test: printing an integer despite invalid format is not supported anymore
 echo
+echo 'Unit test: back compatibility broken'
+echo
+./busybox awk 'BEGIN { CONVFMT="%n"; x=314; print x "" }';
+./busybox awk 'BEGIN { CONVFMT=""; x=314; print x "" }';
+
+# Unit test: support for int 53 bits + sign and pointers added to awk
+echo
+echo 'Unit test: how integers and pointers display'
+echo
+./busybox awk 'BEGIN { CONVFMT="%d"; x=-1; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%u"; x=-1; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="0x%x"; x=-1; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%p"; x=-1; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%p"; x=11; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%x"; x=4294967296; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%lx"; x=4294967296; print x "" }';
+./busybox awk 'BEGIN { CONVFMT="%llx"; x=8589934592; print x "" }';
