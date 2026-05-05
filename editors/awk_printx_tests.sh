a59236653bbcb1d6234daf40e6278c926294cc0c
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-05-05 15:52:38 +0200

    awk: numeric identifiers full recognition w/ %p, stricter +41b tot.
       text	   data	    bss	    dec	    hex	filename
      19338       0       0   19338    4b8a editors/awk.o
    Fixing:                     +51
      19389       0       0   19389    4bbd editors/awk.o
    Extend:                     +16
      19405	      0	      0	  19405	   4bcd	editors/awk.o
    Coding:                      -2
      19403	      0	      0	  19403	   4bcb	editors/awk.o
    Pointers:                   -62
      19341	      0	      0	  19341	   4b8d	editors/awk.o
    Stricter:                   +38
      19379	      0	      0	  19379	   4bb3	editors/awk.o
    Total:                      +41
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- editors/awk_printx_tests.sh
+++ editors/awk_printx_tests.sh
@@ -10,18 +10,19 @@ echo 'Unit test: 3.14 ko...ko 3.14'
 ./busybox awk 'BEGIN { CONVFMT="%"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { OFMT="%"; x=3.14; print x }';
 ./busybox awk 'BEGIN { CONVFMT="%"; x=3.14; print x }'
-echo
+echo '    ---- == 4 == -----'
 ./busybox awk 'BEGIN { OFMT="%nf"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%n"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { OFMT="%nf"; x=3.14; print x }';
 ./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x }'
-echo
+echo '    ---- == 5 == -----'
 ./busybox awk 'BEGIN { OFMT="%%nf"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%%n"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { OFMT="%%nf"; x=3.14; print x }';
 ./busybox awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x }'
+echo '    ---- == 5 == -----'
 
 # Unit test: 3.14 ??
 echo
@@ -31,19 +32,22 @@ echo
 ./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mf"; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mx"; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mm"; x=3.14; print x "" }'
+echo '    ---- == 4 == -----'
 
 # Unit test: see all-and-only the OKs, it is passed otherwise not
 echo
 echo 'Unit test: only 5 OKs expected'
 echo
 ./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%.2f OK f"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%.2fpi %016llx f"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="walk: %.2fpi %016llx "; x=3.14; print x "" }'
+echo '    ---- == 3 == -----'
+./busybox awk 'BEGIN { CONVFMT="%.2f OK f"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="OK pi: %.2fx"; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="%.2fOKpi"; x=3.14; print x "" }'
 ./busybox awk 'BEGIN { CONVFMT="%d OK"; x=3.14; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%f OK p"; x=3.14; print x "" }';
+echo '    ---- == 5 == -----'
 
 # Unit test: printing an integer despite invalid format is not supported anymore
 echo
@@ -51,6 +55,7 @@ echo 'Unit test: back compatibility broken'
 echo
 ./busybox awk 'BEGIN { CONVFMT="%n"; x=314; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT=""; x=314; print x "" }';
+echo '    ---- == 2 == -----'
 
 # Unit test: support for int 53 bits + sign and pointers added to awk
 echo
@@ -60,7 +65,10 @@ echo
 ./busybox awk 'BEGIN { CONVFMT="%u"; x=-1; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="0x%x"; x=-1; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%p"; x=-1; print x "" }';
+echo '    ---- == 4 == -----'
 ./busybox awk 'BEGIN { CONVFMT="%p"; x=11; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%x"; x=4294967296; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%lx"; x=4294967296; print x "" }';
 ./busybox awk 'BEGIN { CONVFMT="%llx"; x=8589934592; print x "" }';
+echo '    ---- == 4 == -----'
+echo
