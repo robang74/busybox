6a5431765b00d481677290c4645267620354c3f5
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-05-05 20:53:27 +0200

    miscutils/unbuffer.c: add 8th draft of unbuffer applet
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- miscutils/unbuffer.c
+++ miscutils/unbuffer.c
@@ -1,9 +1,18 @@
 /*
  * (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, GPLv2 license
+ *
+ *  size check: before vs after:
+ *     text	   data	    bss	    dec	    hex	filename
+ *  1155508	  18124	   2056	1175688	 11f088	busybox
+ *  1156166	  18144	   2056	1176366	 11f32e	busybox
+ *  Total:                     +678
+ *
+ *  how to apply:
+ *    copy this file into in miscutils/unbuffer.
  */
 
 //config:config UNBUFFER
-//config:	bool "unbuffer (0.8kb)"
+//config:	bool "unbuffer (0.7kb)"
 //config:	default n
 //config:	help
 //config:	  Run a command in a PTY to disable buffering.
@@ -16,17 +25,6 @@
 //usage: "CMD [ARGS]"
 //usage:#define unbuffer_full_usage "\n\n"
 //usage: "Run a command in a PTY to disable buffering."
-
-/*
-  size check: before vs after:
-     text	   data	    bss	    dec	    hex	filename
-  1096595	  16691	   1656	1114942	 11033e	busybox
-     text	   data	    bss	    dec	    hex	filename
-  1097330	  16707	   1664	1115701	 11062c	busybox
-
-  how to apply:
-  copy this file into in miscutils/unbuffer.
-*/
 // how to activate:
 // make oldconfig && sed "s/^# *\(CONFIG_UNBUFFER\).*/\\1=y/" -i .config
 
@@ -41,64 +39,74 @@ static pid_t pidfw = 0;
 
 static void sigwinch_handler(int sig UNUSED_PARAM)
 {
-    struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0) {
-        ioctl(ptyfd, TIOCSWINSZ, &ws);
-    }
+	struct winsize ws;
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0) {
+		ioctl(ptyfd, TIOCSWINSZ, &ws);
+	}
 }
 
 static void forward_signals(int sig) {
-    if (pidfw) kill(pidfw, sig);
+	if (pidfw) kill(pidfw, sig);
 }
+
+#define SIGS_BITMASK ((1<<SIGINT)|(1<<SIGTERM)|(1<<SIGHUP)|(1<<SIGQUIT))
 
 int unbuffer_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
 int unbuffer_main(int argc, char **argv)
 {
-    int n, fd;
-    pid_t pid;
-    struct termios tios;
-    sigset_t set, oldset;
-    char ch;
+	int n, fd;
+	pid_t pid;
+	struct termios tios;
+	sigset_t set, oldset;
+	char ch;
 
-    if (!(argc >> 1)) bb_show_usage();
+	if (!(argc >> 1)) bb_show_usage();
 
-    setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
 
-    sigfillset(&set);
-    sigprocmask(SIG_BLOCK, &set, &oldset);
-    signal(SIGWINCH, sigwinch_handler);
-    signal(SIGCHLD, SIG_DFL);
+	sigfillset(&set);
+	sigprocmask(SIG_BLOCK, &set, &oldset);
+	signal(SIGWINCH, sigwinch_handler);
+	signal(SIGCHLD, SIG_DFL);
 
-    if((pid = forkpty(&fd, NULL, NULL, NULL)) < 0)
-        bb_perror_msg_and_die("forkpty");
+	if((pid = forkpty(&fd, NULL, NULL, NULL)) < 0)
+		bb_perror_msg_and_die("forkpty");
 
-    if (pid == 0) { // child proccess
-        sigprocmask(SIG_SETMASK, &oldset, NULL);
-        BB_EXECVP_or_die(argv + 1);
-    } // parent process
-    ptyfd = fd; pidfw = pid;
+	if (pid == 0) { // child proccess
+		sigprocmask(SIG_SETMASK, &oldset, NULL);
+		BB_EXECVP_or_die(argv + 1);
+	} // parent process
+	ptyfd = fd; pidfw = pid;
 
-    #define SIGS_BITMASK ((1<<SIGINT)|(1<<SIGTERM)|(1<<SIGHUP)|(1<<SIGQUIT))
-    bb_signals(SIGS_BITMASK, forward_signals);
-    sigprocmask(SIG_SETMASK, &oldset, NULL);
-    sigwinch_handler(0);
+	bb_signals(SIGS_BITMASK, forward_signals);
+	sigprocmask(SIG_SETMASK, &oldset, NULL);
+	sigwinch_handler(0);
 
-    if (tcgetattr(ptyfd, &tios) == 0) {
-        cfmakeraw(&tios);
-        tios.c_oflag |= ONLCR;
-        tcsetattr(ptyfd, TCSANOW, &tios);
-    }
+// RAF: there is a good chance to find a bb function also for signals
+// TODO: check previous commits with the keyword 'unbuffer' can help.
+#if 0
+	if (tcgetattr(ptyfd, &tios) == 0) {
+		cfmakeraw(&tios);
+		tios.c_oflag |= ONLCR;
+		tcsetattr(ptyfd, TCSANOW, &tios);
+	}
+#else
+//   text	   data	    bss	    dec	    hex	filename
+//    619	      4	      4	    627	    273	miscutils/unbuffer.o
+//    580	      4	      4	    588	    24c	miscutils/unbuffer.o
+	set_termios_to_raw(ptyfd, &tios, 0);
+#endif
 
-    while ((n = read(ptyfd, &ch, 1)) == 1) {
-        if(full_write(STDOUT_FILENO, &ch, 1) != 1) {
-            bb_simple_perror_msg("write");
-            break;
-        }
-    }
-    if (n < 0 && errno != EIO)
-        bb_simple_perror_msg("read");
+	while ((n = read(ptyfd, &ch, 1)) == 1) {
+		if(full_write(STDOUT_FILENO, &ch, 1) != 1) {
+			bb_simple_perror_msg("write");
+			break;
+		}
+	}
+	if (n < 0 && errno != EIO)
+		bb_simple_perror_msg("read");
 
-    close(ptyfd);
+	close(ptyfd);
 
-    return wait_for_exitstatus(pid);
+	return wait_for_exitstatus(pid);
 }
