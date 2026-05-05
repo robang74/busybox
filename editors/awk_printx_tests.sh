1bab2753d2e29b1b7db1c6253863fb35d94795eb
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-05-06 00:04:19 +0200

    awk: invalid conversion handling for a quick debug, +5/+70b tot.
    Undefined behaviour isn't necessarily a failure (exit 1) and printing
    the format as-is received by the users helps them to debug faster.
    Adding a trivial prefix "E?:" helps to catch these cases immediately.
    
       text    data     bss     dec     hex filename
      19338       0       0   19338    4b8a editors/awk.o
    Fixing:                     +51
      19389       0       0   19389    4bbd editors/awk.o
    Extend:                     +16
      19405       0       0   19405    4bcd editors/awk.o
    Coding:                      -2
      19403       0       0   19403    4bcb editors/awk.o
    Pointers:                   -62
      19341       0       0   19341    4b8d editors/awk.o
    Stricter:                   +38
      19379       0       0   19379    4bb3 editors/awk.o
    Debugger:                   +38
      19408       0       0   19408    4bd9 editors/awk.o
    Total:                      +70
    
    Info by Schindler, Dietmar <dietmar.schindler@manrolandgoss.com>
    according to
      - https://pubs.opengroup.org/onlinepubs/9799919799/utilities/awk.html
      - https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/V1_chap05.html
    If any character sequence in the format string begins with a '%' character,
    but does not form a valid conversion specification, the behavior is unspecified
    
    Adding a bit of extra arbitrary behaviour, ignoring the "% d"-like cases seems
    fine when awk is supposed to be used for automation on embedded systems:
    
    !ENABLE_DESKTOP:
      19343       0       0   19343    4b8f editors/awk.o
    Total:                       +5
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- editors/awk_printx_tests.sh
+++ editors/awk_printx_tests.sh
@@ -1,74 +1,79 @@
 #!/bin/sh
 
+runbin=${1:-./busybox}
+
 echo
-echo "Running $0 testing script..."
+echo "Running $0 testing script with:"
+echo "  $runbin"
+echo
 
 # Unit test: 3.14 ko...ko 3.14
 echo
 echo 'Unit test: 3.14 ko...ko 3.14'
-./busybox awk 'BEGIN { OFMT="%"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { OFMT="%"; x=3.14; print x }';
-./busybox awk 'BEGIN { CONVFMT="%"; x=3.14; print x }'
+$runbin awk 'BEGIN { OFMT="%"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { OFMT="%"; x=3.14; print x }'
+$runbin awk 'BEGIN { CONVFMT="%"; x=3.14; print x }'
 echo '    ---- == 4 == -----'
-./busybox awk 'BEGIN { OFMT="%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%n"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { OFMT="%nf"; x=3.14; print x }';
-./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x }'
+$runbin awk 'BEGIN { OFMT="%nf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%n"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { OFMT="%nf"; x=3.14; print x }'
+$runbin awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x }'
 echo '    ---- == 5 == -----'
-./busybox awk 'BEGIN { OFMT="%%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%%n"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { OFMT="%%nf"; x=3.14; print x }';
-./busybox awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x }'
+$runbin awk 'BEGIN { OFMT="%%nf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%%n"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { OFMT="%%nf"; x=3.14; print x }'
+$runbin awk 'BEGIN { CONVFMT="%%nf"; x=3.14; print x }'
 echo '    ---- == 5 == -----'
 
 # Unit test: 3.14 ??
 echo
 echo 'Unit test: 3.14 ??'
 echo
-./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f"; x=3.14; print x "" }'
-./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mf"; x=3.14; print x "" }'
-./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mx"; x=3.14; print x "" }'
-./busybox awk 'BEGIN { CONVFMT="Test PI: %.2f mm"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="Test PI: %.2f"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="Test PI: %.2f mf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="Test PI: %.2f mx"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="Test PI: %.2f mm"; x=3.14; print x "" }'
 echo '    ---- == 4 == -----'
 
 # Unit test: see all-and-only the OKs, it is passed otherwise not
 echo
-echo 'Unit test: only 5 OKs expected'
+echo 'Unit test: only 4 OKs expected'
 echo
-./busybox awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%.2fpi %016llx f"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="walk: %.2fpi %016llx "; x=3.14; print x "" }'
-echo '    ---- == 3 == -----'
-./busybox awk 'BEGIN { CONVFMT="%.2f OK f"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="OK pi: %.2fx"; x=3.14; print x "" }'
-./busybox awk 'BEGIN { CONVFMT="%.2fOKpi"; x=3.14; print x "" }'
-./busybox awk 'BEGIN { CONVFMT="%d OK"; x=3.14; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%f OK p"; x=3.14; print x "" }';
-echo '    ---- == 5 == -----'
+$runbin awk 'BEGIN { CONVFMT="%nf"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%.2fpi %016llx f"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="walk: %.2fpi %016llx "; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%  d"; x=3.14; print x "" }'
+echo '    ---- == 4 == -----'
+$runbin awk 'BEGIN { CONVFMT="%.2f OK f"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="OK pi: %.2fx"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%.2fOKpi"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%f OK p"; x=3.14; print x "" }'
+echo '    ---- == 4 == -----'
 
 # Unit test: printing an integer despite invalid format is not supported anymore
 echo
 echo 'Unit test: back compatibility broken'
 echo
-./busybox awk 'BEGIN { CONVFMT="%n"; x=314; print x "" }';
-./busybox awk 'BEGIN { CONVFMT=""; x=314; print x "" }';
-echo '    ---- == 2 == -----'
+$runbin awk 'BEGIN { CONVFMT=" % f PC?"; x=3.14; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%n"; x=314; print x "" }'
+$runbin awk 'BEGIN { CONVFMT=""; x=314; print x "" }'
+echo '    ---- == 3 == -----'
 
 # Unit test: support for int 53 bits + sign and pointers added to awk
 echo
 echo 'Unit test: how integers and pointers display'
 echo
-./busybox awk 'BEGIN { CONVFMT="%d"; x=-1; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%u"; x=-1; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="0x%x"; x=-1; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%p"; x=-1; print x "" }';
+$runbin awk 'BEGIN { CONVFMT="%d"; x=-1; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%u"; x=-1; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="0x%x"; x=-1; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%p"; x=-1; print x "" }'
 echo '    ---- == 4 == -----'
-./busybox awk 'BEGIN { CONVFMT="%p"; x=11; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%x"; x=4294967296; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%lx"; x=4294967296; print x "" }';
-./busybox awk 'BEGIN { CONVFMT="%llx"; x=8589934592; print x "" }';
+$runbin awk 'BEGIN { CONVFMT="%p"; x=11; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%x"; x=4294967296; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%lx"; x=4294967296; print x "" }'
+$runbin awk 'BEGIN { CONVFMT="%llx"; x=8589934592; print x "" }'
 echo '    ---- == 4 == -----'
 echo
