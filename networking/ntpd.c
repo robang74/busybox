robang74/busybox

commit 85acf71d2579ebe4eec05c6f31901adffa700adc

ntpd: make NTP client and server Y2036/2038-ready
The 32-bit integer part of the NTP timestamp overflows in year 2036,
which starts the second NTP era.

Modify the timestamp conversion to shift values between 1900-1970 (in
the first era) to the second era to enable the client to measure its
offset correctly until year 2106 (assuming 64-bit time_t).

Also update the conversion from double used when stepping the clock to
work with 64-bit time_t after reaching the maximum 32-bit value in 2038
and the server conversion to work correctly in the next NTP era.

function                                             old     new   delta
lfp_to_d                                              51      64     +13
step_time                                            326     332      +6
.rodata                                           105260  105264      +4
d_to_lfp                                             100      86     -14
------------------------------------------------------------------------------
(add/remove: 0/0 grow/shrink: 3/1 up/down: 23/-14)              Total: 9 bytes

Signed-off-by: Miroslav Lichvar <mlichvar@redhat.com>
Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

networking/ntpd.c
@@ -554,7 +554,7 @@ gettime1900d(void)
 static void
 d_to_tv(struct timeval *tv, double d)
 {
-	tv->tv_sec = (long)d;
+	tv->tv_sec = (time_t)d;
 	tv->tv_usec = (d - tv->tv_sec) * 1000000;
 }
 
@@ -565,6 +565,14 @@ lfp_to_d(l_fixedpt_t lfp)
 	lfp.int_partl = ntohl(lfp.int_partl);
 	lfp.fractionl = ntohl(lfp.fractionl);
 	ret = (double)lfp.int_partl + ((double)lfp.fractionl / UINT_MAX);
+	/*
+	 * Shift timestamps before 1970 to the second NTP era (2036-2106):
+	 * int_partl value of OFFSET_1900_1970 (2208988800) is interpreted as
+	 * the start of year 1970 and it is the minimal representable time,
+	 * all values form the sequence 2208988800..0xffffffff,0..2208988799.
+	 */
+	if (lfp.int_partl < OFFSET_1900_1970)
+		ret += (double)(1ULL << 32); /* because int_partl is 32-bit wide */
 	return ret;
 }
 static NOINLINE double
@@ -582,8 +590,8 @@ d_to_lfp(l_fixedpt_t *lfp, double d)
 {
 	uint32_t intl;
 	uint32_t frac;
-	intl = (uint32_t)d;
-	frac = (uint32_t)((d - intl) * UINT_MAX);
+	intl = (uint32_t)(time_t)d;
+	frac = (uint32_t)((d - (time_t)d) * UINT_MAX);
 	lfp->int_partl = htonl(intl);
 	lfp->fractionl = htonl(frac);
 }

commit 3112d72f806c339b82e0496c41472ec419e55c5f

ntpd: avoid time_t overflow in d_to_lfp on 32-bit systems
Commit 85acf71d2579 changed d_to_lfp() to cast the NTP timestamp through
time_t before converting it to the 32-bit NTP seconds field.

This breaks systems where time_t is signed 32-bit. Current NTP seconds are
already larger than INT32_MAX, since NTP timestamps count seconds since
1900-01-01. On such systems the double-to-time_t conversion is out of range
and can produce invalid transmit/reference timestamps or crash ntpd server
mode.

Avoid time_t in d_to_lfp(). Wrap timestamps after the first NTP era before
converting to uint32_t, matching the era handling already used by lfp_to_d().

This fixes ntpd server mode on FreshTomato/uClibc 32-bit MIPS/ARM targets
without adding int64_t code.

Fixes: 85acf71d2579 ("ntpd: make NTP client and server Y2036/2038-ready")

Signed-off-by: pedro311 <pedro311@gmail.com>

networking/ntpd.c
@@ -583,8 +583,10 @@ d_to_lfp(l_fixedpt_t *lfp, double d)
 {
 	uint32_t intl;
 	uint32_t frac;
-	intl = (uint32_t)(time_t)d;
-	frac = (uint32_t)((d - (time_t)d) * 0xffffffff);
+	if (d >= (double)(1ULL << 32))
+		d -= (double)(1ULL << 32);
+	intl = (uint32_t)d;
+	frac = (uint32_t)((d - intl) * 0xffffffff);
 	lfp->int_partl = htonl(intl);
 	lfp->fractionl = htonl(frac);
 }