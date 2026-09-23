0c852c5d6596646561c1af517759b6ef2601d9c6
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-09-23 16:14:25 +0200

    chunked transfer conflicts with a user-supplied Content-Length, fix v2
    The collision between chunked transfer and user-supplied Content-Length
    exists only on the DESKTOP branch. When reading from STDIN in chunked
    mode, the check cannot be performed, but this is an intrinsic limitation
    of streaming from pipes, not a bug.
    
       text    data     bss     dec     hex filename
       8763       0       0    8763    223b networking/wget.o
       8880       0       0    8880    22b0 networking/wget.o v1
                               +117
       8830       0       0    8830    227e networking/wget.o v2
                                +67
    
    Side effect: block Slow POST via Content-Length override
    
    Checking chunked transfer at send time (v2) allowes small files with
    inflated Content-Length to hang servers. Checking file size upfront in
    wget_main() instead: costs 50 bytes (v1), but prevents Slow POST DoS.
    
    The v1 allows the Slow POST DoS but only when a file is bigger than
    two megabytes which is a very inefficient way for doing a DoS due to
    the ration between transfer time and server socket hanging-wasted.
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- networking/wget.c
+++ networking/wget.c
@@ -1376,6 +1376,13 @@ plain_request:
 			}
 	#if ENABLE_DESKTOP
 			if(G.post_file_fd >= 0) {
+				/* Chunked transfer conflicts with a user-supplied Content-Length */
+				if (G.user_headers & HDR_CONTENT_LENGTH) {
+					bb_error_msg_and_die(
+						"Content-Length conflicts with chunked transfer"
+					);
+				}
+
 				/* More to read in the post file -> chunked transfer */
 				int chunk_bytes = G.post_data_len;
 				SENDFMT(sfp, "Transfer-Encoding: chunked\r\n\r\n%x\r\n", chunk_bytes);
@@ -1781,18 +1788,6 @@ IF_DESKTOP(	"no-parent\0"        No_argument       "\xf0")
 			xdup2(G.log_fd, STDERR_FILENO);
 		}
 	}
-
-#if ENABLE_DESKTOP && ENABLE_FEATURE_WGET_LONG_OPTIONS
-	/* Chunked transfer conflicts with a user-supplied Content-Length */
-	if (G.post_file
-	&& NOT_LONE_DASH(G.post_file)
-	&& (G.user_headers & HDR_CONTENT_LENGTH)
-	){
-		struct stat st;
-		if (!stat(G.post_file, &st) && st.st_size >= POST_CHUNK_BYTES)
-			bb_error_msg_and_die("Content-Length conflicts with chunked transfer");
-	}
-#endif
 
 	while (*argv)
 		download_one_url(*argv++);
